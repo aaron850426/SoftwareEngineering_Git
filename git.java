a1a990ba7caaf80995a8ef20d9725061fef09cef
i11007020 <i11007020@gmail.com>
2017-10-25 13:07:00 +0800

    Update git.java

--- git.java
+++ git.java
@@ -3,5 +3,8 @@ public class git {
     System.out.println("我覺得可以");
     System.out.println("Bence yapabilirim");
     System.out.println("我觉得可以");
+    System.out.println("Nik uste dut");
+    System.out.println("Whakaaro ahau ka taea e ahau");
+    System.out.println("Mi pensas, ke mi povas");
   }
 }
