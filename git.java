55fc5d154a6944b7148a1265d5980beff9cb2aa0
aaron850426 <aaron850426@gmail.com>
2017-10-25 13:04:01 +0800

    Update git.java

--- git.java
+++ git.java
@@ -1,6 +1,7 @@
 public class git {
   public static main(String[] args) {
-    System.out.println("Test");
-    System.out.println("Test２");
+    System.out.println("我覺得可以");
+    System.out.println("Bence yapabilirim");
+    System.out.println("我觉得可以");
   }
 }
