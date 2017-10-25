eb5ddfe92280a0efaf8142f37e15b777065f7195
ji394su30319 <ji394su30319@yahoo.com.tw>
2017-10-25 13:20:14 +0800

    Update git.java

--- git.java
+++ git.java
@@ -1,16 +1,13 @@
 public class git {
   public static main(String[] args) {
-<<<<<<< HEAD
     System.out.println("אני חושב שאני יכול");
     System.out.println("Manaʻo wau hiki iaʻu");
-	System.out.println("TԿարծում եմ, կարող եմ");
-=======
+    System.out.println("Կարծում եմ, կարող եմ");
     System.out.println("我覺得可以");
     System.out.println("Bence yapabilirim");
     System.out.println("我觉得可以");
     System.out.println("Nik uste dut");
     System.out.println("Whakaaro ahau ka taea e ahau");
     System.out.println("Mi pensas, ke mi povas");
->>>>>>> a1a990ba7caaf80995a8ef20d9725061fef09cef
   }
 }
