a273a2c433b3895c9b196aaf906dbe05373a6434
GraphicLab503 <ji394su30319@gmail.com>
2017-10-25 13:04:44 +0800

    test1

--- git.java
+++ git.java
@@ -1,6 +1,7 @@
 public git {
   public static main(String[] args) {
-    System.out.println("Test");
-    System.out.println("Test２");
+    System.out.println("Test0 from 00357103");
+    System.out.println("Test1 from 00357103");
+	System.out.println("Test2 from 00357103");
   }
 }
